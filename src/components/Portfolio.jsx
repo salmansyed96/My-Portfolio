c229582c7dbf30deb19ff4ff7843bb3b1668c1f0
salmansyed96 <124539970+salmansyed96@users.noreply.github.com>
2023-08-02 16:47:40 +0530

    link add

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,54 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/Amazonn.png";
-import installNode from "../assets/portfolio/G-F.jpeg";
-import navbar from "../assets/portfolio/swiggy2.jpg";
-import reactParallax from "../assets/portfolio/h&m-web.jpeg";
-import reactSmooth from "../assets/portfolio/DIgital .png";
-import reactWeather from "../assets/portfolio/TODo.jpeg";
+import AmazonUi from "../assets/portfolio/Amazonn.png";
+import GOogleForm from "../assets/portfolio/G-F.jpeg";
+import Swiggy from "../assets/portfolio/swiggy2.jpg";
+import HandM from "../assets/portfolio/h&m-web.jpeg";
+import DIgitalMove from "../assets/portfolio/DIgital .png";
+import ToDoList from "../assets/portfolio/TODo.jpeg";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct, 
+      src: AmazonUi, 
+      link: 'https://amazon-ui-8717.vercel.app/',
+      code: 'https://github.com/salmansyed96/portfolio-AmazonClone-responsive-master'
       
     },
     {
       id: 2,
-      src: reactParallax,
+      src: HandM,
+      link: 'https://h-and-m-website-clone.vercel.app/',
+      code: 'https://github.com/salmansyed96/H-M-website-clone'
+
     },
     {
       id: 3,
-      src: navbar,
+      src: Swiggy,
+      link: 'https://swiggy-ui-iota.vercel.app/',
+      code: 'https://github.com/salmansyed96/Swiggy-Ui'
+
     },
     {
       id: 4,
-      src: reactSmooth,
+      src: DIgitalMove,
+      link: 'https://reactmultipage-main.vercel.app/',
+      code: 'https://github.com/salmansyed96/reactmultipage-main'
+
     },
     {
       id: 5,
-      src: installNode,
+      src: GOogleForm,
+      link: 'https://g-form-roan.vercel.app/',
+      code: 'https://github.com/salmansyed96/G-Form'
+
     },
     {
       id: 6,
-      src: reactWeather,
+      src: ToDoList,
+      link: 'https://to-do-list-react-js-puce.vercel.app/',
+      code: 'https://github.com/salmansyed96/ToDo-List-React.js'
+
     },
   ];
 
@@ -49,7 +66,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, link, code }) => (
             <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -57,14 +74,13 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a href={link} className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" target="_blank" rel="noreferrer">
                   
                   Demo
-                </button>
-                <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
+                </a>
+                <a href={code}  className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105" target="_blank" rel="noreferrer">
                   Code
-                </button>
-                
+                </a>
               </div>
               {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos in commodi officia laudantium corrupti quam!</p> */}
             </div>
